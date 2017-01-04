5e155e9a2919f35c86cf5b521344dd22c5c60335
Tomasz Koter <Tomasz Koter>
2017-01-04 21:39:10 +0100

    beginning k3m

---
+++ scripts/k3m.m
@@ -0,0 +1,29 @@
+function outputImage = k3m(image)
+  
+  
+  neighborMask = [128 1   2; ...
+                  64  0   4; ...
+                  32  16  8];
+  
+  
+  change = 1;
+  while change
+    
+    % phase 0
+    
+    % phase 1
+    
+    % phase 2
+    
+    % phase 3
+    
+    % phase 4
+    
+    % phase 5
+    
+    % phase 6
+    
+  end
+  
+  % 1-pixel width phase
+end   
